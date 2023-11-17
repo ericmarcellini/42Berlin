88ec766bc0eed7fa83e33c6b57cc1f8ea8b0b2ce
Eric Martinez Marcellini <ermarti2@c4b6c3.42berlin.de>
2023-11-17 17:57:44 +0100

    ft memcmp

--- 42p/libft/ft_memcmp.c
+++ 42p/libft/ft_memcmp.c
@@ -6,7 +6,26 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:15:02 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 13:15:03 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/17 17:56:28 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*string1;
+	unsigned char	*string2;
+	size_t			i;
+
+	string1 = (unsigned char) *s1;
+	string2 = (unsigned char) *s2;
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char) string1[i] != (unsigned char) string2[i])
+			return ((unsigned char) string1[i] - (unsigned char) string2[i]);
+		i++;
+	}
+	return (0);
+}
