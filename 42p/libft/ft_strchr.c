975e40ff98fe5d570c0728e312b38a62a2173bc1
Eric Martinez Marcellini <ermarti2@c4b6c3.42berlin.de>
2023-11-17 15:39:13 +0100

    first commit from pc

--- 42p/libft/ft_strchr.c
+++ 42p/libft/ft_strchr.c
@@ -6,7 +6,26 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:14:07 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 13:14:07 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:03:53 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+	char			cc;
+
+	cc = (char) c;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == cc)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == cc)
+		return ((char *) &s[i]);
+	return (NULL);
+}
--- 42p/libft/libft.h
+++ 42p/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:39:27 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 16:40:38 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:19:56 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define LIBFT_H
 # include <stdlib.h>
 # include <unistd.h>
+
+size_t ft_strlen(const char *s);
 
 #endif
