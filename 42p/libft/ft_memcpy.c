a10de5d3e84978e9526f3d7fbd33a798eff3128d
Eric Martinez Marcellini <ermarti2@c4b6c3.42berlin.de>
2023-11-17 18:40:01 +0100

    strlcpy start

--- 42p/libft/ft_memcpy.c
+++ 42p/libft/ft_memcpy.c
@@ -6,7 +6,25 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:13:10 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 13:13:11 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:08:15 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*destination;
+	unsigned char	*source;
+
+	if (dst == (void *) 0 && src == (void *) 0)
+		return (dst);
+	destination = (unsigned char *)dst;
+	source = (unsigned char *)src;
+	while (n > 0)
+	{
+		*(destination++) = *(source++);
+		n--;
+	}
+	return (dst);
+}
--- 42p/libft/ft_strlcpy.c
+++ 42p/libft/ft_strlcpy.c
@@ -6,7 +6,22 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:13:32 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 13:13:33 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:37:49 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+// ITS WRONG
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < dstsize - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (i);
+}
