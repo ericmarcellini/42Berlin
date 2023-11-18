f8e7a95a3cd3d12794fe5502a15575878543b78e
Eric Martinez Marcellini <ermarti2@c4b6c5.42berlin.de>
2023-11-18 11:48:51 +0100

    pseudocode

--- 42p/libft/ft_strlcpy.c
+++ 42p/libft/ft_strlcpy.c
@@ -6,12 +6,12 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:13:32 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/17 18:37:49 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/18 11:43:25 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// ITS WRONG
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
@@ -22,6 +22,37 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
+	if (i < dstsize)
+		dst[i] = '\0';
+	while (src[i] != '\0')
+		i++;
 	return (i);
 }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char dst[20]; // Destination buffer size for testing
+//     const char *test_strings[] = {
+//         "Hello",            // Short string
+//         "This is a test",   // Longer string
+//         "",                 // Empty string
+//         "Long destination buffer test" // Destination buffer > source string length
+//     };
+
+//     for (int i = 0; i < sizeof(test_strings) / sizeof(test_strings[0]); ++i) {
+//         const char *src = test_strings[i];
+//         size_t src_len = strlen(src);
+//         size_t result = ft_strlcpy(dst, src, sizeof(dst));
+
+//         printf("Test %d:\n", i + 1);
+//         printf("Source: '%s'\n", src);
+//         printf("Returned Length: %zu\n", result);
+//         printf("Copied String: '%s'\n", dst);
+//         printf("Expected Length: %zu\n", src_len);
+//         printf("---------------------------------------------------\n");
+//     }
+
+//     return 0;
+// }
--- 42p/libft/ft_strncmp.c
+++ 42p/libft/ft_strncmp.c
@@ -6,7 +6,23 @@
 /*   By: ermarti2 <ermarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:14:39 by ermarti2          #+#    #+#             */
-/*   Updated: 2023/11/14 13:14:39 by ermarti2         ###   ########.fr       */
+/*   Updated: 2023/11/18 11:48:12 by ermarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    /* loop over both string until we reach n characters */
+    /* check if current s1 character is different than
+     * current s2 character
+     */
+         /* if characters are different, return the difference
+          * between both characters
+          */
+      /* if we read both strings until n characters and no difference
+       * were found, return 0 as there is no difference
+       */
+
+}
